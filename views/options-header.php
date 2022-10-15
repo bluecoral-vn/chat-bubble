9af6a6f13b1b62a797c065a5d6cb285ed0a5ed04
HoNgocKinh <hongockinh69@gmail.com>
2022-10-15 20:32:56 +0700

    Hide Chat bubble title menu

--- views/options-header.php
+++ views/options-header.php
@@ -10,6 +10,6 @@ if (!defined('ABSPATH')) exit;
 global $cbb_options; 
 extract($cbb_options); ?>
 
-	<div class="col-md-12">
+	<!-- <div class="col-md-12">
 	<a href="http://cyberbase.vn/?utm_source=chat-bubble"><img class="ufa-logo" src="<?php echo plugins_url($this->domain.'/assets/images/logo.png'); ?>" alt="<?php _e('Chat Bubble', 'chat-bubble'); ?>" /></a>
-	</div>
+	</div> -->
